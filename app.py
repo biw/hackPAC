f6871cd1b6a8f3e6bb9929ed2286c4519edcb38b
719Ben <ben.williams@colorado.edu>
2016-06-18 00:07:46 -0700

    Cleaned up app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-#----------------------------------------------------------------------------#
-# Imports
-#----------------------------------------------------------------------------#
+import forms  # this is the local file forms.py
 
-from flask import Flask, render_template, request
 import sqlite3
 import logging
-from logging import Formatter, FileHandler
-from forms import *
 import os
 
-conn = sqlite3.connect('database.db')
+from werkzeug.serving import run_simple
+from flask import Flask, render_template, request
+from logging import Formatter, FileHandler
+
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -17,27 +15,20 @@ conn = sqlite3.connect('database.db')
 
 app = Flask(__name__)
 app.config.from_object('config')
-#db = SQLAlchemy(app)
-
-# Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
+conn = sqlite3.connect('database.db')
 
 # Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
+#
+# def login_required(test):
+#    @wraps(test)
+#    def wrap(*args, **kwargs):
+#        if 'logged_in' in session:
+#            return test(*args, **kwargs)
+#        else:
+#            flash('You need to login first.')
+#            return redirect(url_for('login'))
+#    return wrap
+#
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
@@ -55,28 +46,28 @@ def about():
 
 @app.route('/login')
 def login():
-    form = LoginForm(request.form)
+    form = forms.LoginForm(request.form)
     return render_template('forms/login.html', form=form)
 
 
 @app.route('/register')
 def register():
-    form = RegisterForm(request.form)
+    form = forms.RegisterForm(request.form)
     return render_template('forms/register.html', form=form)
 
 
 @app.route('/forgot')
 def forgot():
-    form = ForgotForm(request.form)
+    form = forms.ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
 
 
 @app.route("/api/upvote")
 def upvote():
+    return None
 
-    # Error handlers.
 
-
+# Error handlers.
 @app.errorhandler(500)
 def internal_error(error):
     # db_session.rollback()
@@ -87,28 +78,10 @@ def internal_error(error):
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
 #----------------------------------------------------------------------------#
 # Launch.
 #----------------------------------------------------------------------------#
 
-# Default port:
 if __name__ == '__main__':
-    app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    run_simple("0.0.0.0", 5000, app,
+               use_reloader=True, use_debugger=True, use_evalex=True)
