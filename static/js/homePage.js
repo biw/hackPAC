dfa7c421aebecb0f4456f7cefbcc06471b3d2e48
Meia Alsup <meia.alsup@gmail.com>
2016-06-18 08:43:09 -0700

    moving scripts to separate file

--- static/js/homePage.js
+++ static/js/homePage.js
@@ -1,5 +1,5 @@
 var loggedInStatus = true;
-var currentUserId = "meia";
+var currentUserId = "sampleUserLogIn";
 var selectedArticle = {iden:"a", upvotes:500, longDescription:"wowowowow number 1 this should be longer example example :) ", header:"Sample Top"};
 var article1 = {iden:"b", upvotes:20, longDescription:"heyy this should be longer example example :) ", header:"Sample 1"};
 var article2 = {iden:"c", upvotes:300, longDescription:"hahahahha omg how do you write long descriptions wau this is an amazing barrier to entry love it ", header:"Sample 2"};
@@ -7,6 +7,72 @@ var articleList = [article1, article2];
 
 $(document).ready(function(){
 	updateHeaderBar(loggedInStatus);
+
+	// show extended descriptions
+	$(document.body).on("click", "sub-post.sub-post-min>p",function() {
+
+	    // get the iframe and the hidden_src
+	    var iframe = $(this).prev()
+	    var hidden_src = iframe.attr("hidden-src")
+
+	    //add the iframe items, and show it
+	    iframe.addClass("article-frame")
+	    iframe.removeClass("hidden-frame")
+	    iframe.attr("src", hidden_src)
+
+	    //replace the paragraph text to close it
+	    var p_text = $(this).html()
+	    p_text = p_text.replace("+ Open", "- Close")
+	    $(this).html(p_text)
+
+	    //switch the view class
+	    $(this).parent().removeClass("sub-post-min")
+	    $(this).parent().addClass("sub-post-max")
+	})
+
+	// hide extended desriptions
+	$(document.body).on("click", "upvote",function() {
+
+	    // get the iframe and the hidden_src
+	    var articleholder = $(this).prev();
+
+	    // //add the iframe items, and show it
+	    // iframe.addClass("hidden-frame")
+	    // iframe.removeClass("article-frame")
+	    // iframe.attr("src", "")
+
+	    // //replace the paragraph text to close it
+	    // var p_text = $(this).html()
+	    // p_text = p_text.replace("- Close", "+ Open")
+	    // $(this).html(p_text)
+
+	    // //switch the view class
+	    // $(this).parent().removeClass("sub-post-max")
+	    // $(this).parent().addClass("sub-post-min")
+	})
+
+	// upvote
+	$(document.body).on("click", "sub-post.sub-post-max>p",function() {
+
+	    // get the iframe and the hidden_src
+	    var iframe = $(this).prev()
+
+	    //add the iframe items, and show it
+	    iframe.addClass("hidden-frame")
+	    iframe.removeClass("article-frame")
+	    iframe.attr("src", "")
+
+	    //replace the paragraph text to close it
+	    var p_text = $(this).html()
+	    p_text = p_text.replace("- Close", "+ Open")
+	    $(this).html(p_text)
+
+	    //switch the view class
+	    $(this).parent().removeClass("sub-post-max")
+	    $(this).parent().addClass("sub-post-min")
+	})
+
+
 	if (selectedArticle) {
 		displayArticle(selectedArticle);
 	}
@@ -17,6 +83,8 @@ $(document).ready(function(){
 		}
 	}
 });
+
+
 
 // true: logged in
 // false: not logged in
